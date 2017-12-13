e8afc12af68df69b00d1ae6c4544353f29191067
Qezz <kezvision@gmail.com>
2017-12-13 21:34:16 +0300

    add inplace string reverse funciton

--- chardevice.c
+++ chardevice.c
@@ -47,6 +47,36 @@ static bool output_once = false;
 static char scancode[1];
 // }
 
+#include <string.h> // for strlen()
+
+// reverse the given null-terminated string in place
+void inplace_reverse(char * str)
+{
+	if (str)
+	{
+		char * end = str + strlen(str) - 1;
+
+		// swap the values in the two given variables
+		// XXX: fails when a and b refer to same memory location
+#   define XOR_SWAP(a,b) do	  \
+		{ \
+			a ^= b; \
+			b ^= a; \
+			a ^= b; \
+		} while (0)
+
+		// walk inwards from both ends of the string,
+		// swapping until we get to the middle
+		while (str < end)
+		{
+			XOR_SWAP(*str, *end);
+			str++;
+			end--;
+		}
+#   undef XOR_SWAP
+	}
+}
+
 struct file * file_open(const char *path, int flags, int rights)
 {
 	struct file * filp = NULL;
